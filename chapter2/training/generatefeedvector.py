193c1d8cbd09147743ebf25c5341d8a1eff1a147
Juan Bermúdez Roldán <juan.roldan.brz@gmail.com>
2017-10-11 18:28:51 +0200

    OK

---
+++ chapter2/training/generatefeedvector.py
@@ -0,0 +1,67 @@
+import feedparser
+import re
+
+from tqdm import tqdm
+
+
+def getwordcounts(url):
+    d = feedparser.parse(url)
+    wc = {}
+    # Loop over all the entries
+    for e in d.entries:
+        if 'summary' in e:
+            summary = e.summary
+        else:
+            summary = e.description
+        # Extract a list of words
+        words = getwords(e.title + ' ' + summary)
+        for word in words:
+            wc.setdefault(word, 0)
+            wc[word] += 1
+    return d.feed.title, wc
+
+
+def getwords(html):
+    txt = re.compile(r'<[^>]+>').sub('', html)
+    # Split words by all non-alpha characters
+    words = re.compile(r'[^A-Z^a-z]+').split(txt)
+    # Convert to lowercase
+    return [word.lower() for word in words if word != '']
+
+if __name__ == "__main__":
+    apcount = {}
+    wordcounts = {}
+    feedlist = [line for line in open('feedlist.txt')]
+    tqdm_feeds = tqdm(feedlist)
+    for feedurl in tqdm_feeds:
+        tqdm_feeds.set_description('Processing %s' % feedurl)
+        try:
+            title, wc = getwordcounts(feedurl)
+            wordcounts[title] = wc
+            for word, count in wc.items():
+                apcount.setdefault(word, 0)
+                if count > 1:
+                    apcount[word] += 1
+        except AttributeError:
+            print('Failed to parse feed %s' % feedurl)
+            continue
+
+
+    wordlist = []
+    for w, bc in apcount.items():
+        frac = float(bc) / len(feedlist)
+        if frac > 0.1 and frac < 0.5:
+            wordlist.append(w)
+
+    out = open('blogdata.txt', 'w')
+    out.write('Blog')
+    for word in wordlist: out.write('\t%s' % word)
+    out.write('\n')
+    for blog, wc in wordcounts.items():
+        out.write(blog)
+        for word in wordlist:
+            if word in wc:
+                out.write('\t%d' % wc[word])
+            else:
+                out.write('\t0')
+        out.write('\n')
